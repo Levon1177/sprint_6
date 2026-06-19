17375aa07a29d9853e4f21839df4d1e040583316
Levon Petrovskiy <levonpetrovskiy@mail.ru>
2026-06-20 01:30:53 +0300

    sprint-6

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ProcessData(input string) (string, error) {
+	trimmed := strings.TrimSpace(input)
+	if trimmed == "" {
+		return "", errors.New("пустые данные для конвертации")
+	}
+
+	if strings.Contains(trimmed, ".") || strings.Contains(trimmed, "-") {
+		return morse.ToText(trimmed), nil
+	}
+
+	return morse.ToMorse(trimmed), nil
+}
